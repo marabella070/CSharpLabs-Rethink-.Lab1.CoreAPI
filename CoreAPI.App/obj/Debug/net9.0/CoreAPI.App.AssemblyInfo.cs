e948e64d4c44d20202d51177c2bbd4ac439ca766
marabella070 <marabella070@gmail.com>
2025-03-28 13:23:59 +0700

    Changes:
    	- Changed the project structure (added the ReflectionHelper Core.Helpers)
    
    	- Generated a class diagram for CoreAPI

--- CoreAPI.App/obj/Debug/net9.0/CoreAPI.App.AssemblyInfo.cs
+++ CoreAPI.App/obj/Debug/net9.0/CoreAPI.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreAPI.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec56aa0d833c90d292216ee573b9e509bfdbce5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a9a7aa542dae1413f836dfef22b7b8c279ed69")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreAPI.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreAPI.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
